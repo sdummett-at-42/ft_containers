5aa0763b68b65876a2521b9a2eb68785120de212
Stone <stone.dummett@gmail.com>
2022-09-06 17:28:36 +0200

    Some work on vector, some member function is partially implemented

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -6,32 +6,59 @@
 /*   By: sdummett <sdummett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/16 14:08:35 by sdummett          #+#    #+#             */
-/*   Updated: 2022/09/05 12:28:09 by sdummett         ###   ########.fr       */
+/*   Updated: 2022/09/06 17:25:38 by sdummett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include <iostream>
 
 # include "vector.hpp"
-# include "map.hpp"
-# include "stack.hpp"
-# include "iterator_traits.hpp"
-# include "reverse_iterator.hpp"
-# include "enable_if.hpp"
-# include "is_integral.hpp"
-# include "equal.hpp"
-# include "lexicographical_compare.hpp"
-# include "pair.hpp"
-# include "make_pair.hpp"
+// # include "map.hpp"
+// # include "stack.hpp"
+// # include "iterator_traits.hpp"
+// # include "reverse_iterator.hpp"
+// # include "enable_if.hpp"
+// # include "is_integral.hpp"
+// # include "equal.hpp"
+// # include "lexicographical_compare.hpp"
+// # include "pair.hpp"
+// # include "make_pair.hpp"
 
-using namespace ft;
+#include <vector>
 
 int main() {
 
-	std::pair<int, char> pr = std::make_pair<int, char>(42, 'S');
-	std::cout << "pr " << pr.first << " " << pr.second << "\n";
+	// int ft = 42;
+	// std::vector<int> vec(42, ft);
+	// int i = 24;
+	// int j = 42;
+	ft::vector<int> vectest(2, 5);
 
-	ft::pair<int, char> ftpr = ft::make_pair<int, char>(42, 'S');
-	std::cout << "ftpr " << ftpr.first << " " << ftpr.second << "\n";
+	// std::cout << ftvec[0] << ftvec[23] << "\n"; 	
+
+
+	// std::allocator<int> allo;
+	// std::allocator<int>::pointer p = allo.allocate(5);
+	// for (int i = 0; i < 5; ++i) {
+	// 	allo.construct(p + i, i * 100);
+	// }
+	// for (int i = 0; i < 5; ++i) {
+	// 	std::cout << "arr[" << i << "] : " << *(p + i) << "\n";
+	// }
+
+	// std::cout << "Reallocating 5...\n";
+	// std::allocator<int>::pointer p2 = allo.allocate(5);
+	// // std::cout << "p : " << p << " p2 : " << p2 << "\n";
+	// std::cout << "5 allocated\n";
+	// for (int i = 0; i < 5; ++i) {
+	// 	allo.construct(p2 + i, i * 10);
+	// }
+	// for (int i = 0; i < 5; ++i) {
+	// 	std::cout << "arr[" << i << "] : " << *(p + i) << "\n";
+	// }
+	// for (int i = 0; i < 5; ++i) {
+	// 	std::cout << "arr[" << i << "] : " << *(p2 + i) << "\n";
+	// }
+
 	return 0;
 }
--- srcs/vector.hpp
+++ srcs/vector.hpp
@@ -6,14 +6,15 @@
 /*   By: sdummett <sdummett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/16 14:12:03 by sdummett          #+#    #+#             */
-/*   Updated: 2022/09/05 16:44:40 by sdummett         ###   ########.fr       */
+/*   Updated: 2022/09/06 17:27:41 by sdummett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # ifndef VECTOR_HPP
 # define VECTOR_HPP
 
-// #include <memory>
+#include <memory>
+#include <cstddef>
 #include "iterator_traits.hpp"
 #include "reverse_iterator.hpp"
 
@@ -22,40 +23,64 @@ namespace ft {
 	template < class T, class Alloc = std::allocator<T> >
 	class vector {
 		public:
-			/* Typedefs */
+			/* --- Typedefs --- */
 			typedef Alloc allocator_type;
 			typedef size_t size_type;
 			typedef T value_type;
+			typedef std::ptrdiff_t difference_type;
 			typedef typename allocator_type::reference reference;
 			typedef typename allocator_type::const_reference const_reference;
 			typedef typename allocator_type::pointer pointer;
 			typedef typename allocator_type::const_pointer const_pointer;
-
-			/* Constructors */
+			/* Still need to add typedefs from documentations (iterators 
+			   for example) */
+			
+			/* --- Constructors --- */
+			// Constructs an empty container, with no elements.
 			explicit vector (const allocator_type& alloc = allocator_type()) : 
-				alloc(alloc),
-	       			n(0) {}
+				_alloc(alloc),
+	       		_size(0),
+				_capacity(0) { std::cout << "Default constructor called\n";}
 
-			explicit vector (size_type n, const value_type& val = value_type(), \
-					const allocator_type& alloc = allocator_type()) {
-				this->arr = alloc.allocate(n);
-				for (int i = 0; i < n; ++i) {
-					arr[i] = val[i];
+			// Constructs a container with n elements. Each element is a copy 
+			// of val.
+			 explicit vector (const size_type& n, const value_type& val = value_type(), 
+            	const allocator_type& alloc = allocator_type()) :
+				_alloc(alloc),
+				_size(0),
+				_capacity(0) {
+				// Notes: Maybe reserving a bit more so we dont have to allocate
+				// memory frequently
+				std::cout << "This constructor is called\n";
+				reserve(n);
+				for (size_type i = 0; i < n; ++i) {
+					_alloc.construct(_p + i, val);
+				}
+				_size = n;
+				std::cout << "Elements contained\n";
+				for (int i = 0; i < _size; i++) {
+					std::cout << " - " << *(_p + i) << "\n";
 				}
 			}
 
+			// Constructs a container with as many elements as the range [first,
+			// last), with each element constructed from its corresponding 
+			// element in that range, in the same order.
 			template <class InputIterator>
 			vector (InputIterator first, InputIterator last, \
 				const allocator_type& alloc = allocator_type());
 
+			// Constructs a container with a copy of each of the elements in x, in the same order.
 			vector (const vector& x);
 
-
-			/* Destructor */
-			~vector();
-
-			/* operator= */
+			/* --- operator= --- */
 			vector& operator= (const vector& x);
+
+			/* --- Destructor --- */
+			~vector() {
+				empty();
+				_alloc.deallocate(_p, _capacity);
+			}
 
 			// /* Iterators */
 			// iterator begin();
@@ -67,15 +92,72 @@ namespace ft {
 			// reverse_iterator rend();
 			// const_reverse_iterator rend() const;
 
-			/* Capacity */
-			size_type size() const;
-			size_type max_size() const;
-			void resize (size_type n, value_type val = value_type());
-			size_type capacity() const;
-			bool empty() const;
-			void reserve (size_type n);
+			/* --- Capacity --- */
+			// Returns the number of elements in the container
+			size_type size() const {
+				return _size;
+			}
+			
+			// Returns the maximum number of elements the container is able to
+			// hold due to system or library implementation limitations
+			size_type max_size() const {
+				return _alloc.max_size();
+			}
+			
+			// Resizes the container to contain count elements. 
+			void resize (size_type n, value_type val = value_type()) {
+				// Notes: check what is the default value
+				// Is the one given at construction
+				if (n > _capacity)
+					reserve(n);
+				if (n < _size) {
+					for (size_type i = n; i < _size; i++) {
+						_alloc.destroy(_p + i);
+					}
+					_size = n;
+				}
+				else if (n > _size) {
+					for (size_type i = _size; i < n; i++) {
+						_alloc.construct(_p + i, val);
+					}
+					_size = n;
+				}
+			}
 
-			/* Element access */
+			// Returns the number of elements that the container has currently
+			// allocated space for. 
+			size_type capacity() const {
+				return _capacity;
+			}
+			
+			// Checks if the container has no elements
+			bool empty() const {
+				if (_size == 0)
+					return true;
+				return false;
+			}
+
+			void reserve (size_type new_cap) {
+				// if less or equal than the actual
+				// capacity, no need to reallocate memory
+				if (new_cap <= _capacity)  
+					return ;
+				if (new_cap > _alloc.max_size()) {
+					// throw an error :
+					// std::length_error if new_cap > max_size(). 
+					return ;
+				}
+				pointer tmp_p = _p;
+				_p = _alloc.allocate(new_cap);
+				_capacity = new_cap;
+				for (size_type i = 0; i < _size; i++) {
+					_alloc.construct(_p + i, *(tmp_p + i));
+					_alloc.destroy(tmp_p + i);
+				}
+				_alloc.deallocate(tmp_p);
+			}
+
+			/* --- Element access --- */
 			reference operator[] (size_type n);
 			const_reference operator[] (size_type n) const;
 			reference at (size_type n);
@@ -85,34 +167,40 @@ namespace ft {
 			reference back();
 			const_reference back() const;
 			
-			/* Modfiers */
+			/* --- Modfiers --- */
 			// template <class InputIterator>
 			// // void assign (InputIterator first, InputIterator last);
 			void assign (size_type n, const value_type& val);
 			void push_back (const value_type& val);
 			void pop_back();
-			// // iterator insert (iterator position, const value_type& val);
+			// iterator insert (iterator position, const value_type& val);
 			// void insert (iterator position, size_type n, const value_type& val);
 			// template <class InputIterator>
-			// // // void insert (iterator position, InputIterator first, InputIterator last);
-			// // iterator erase (iterator position);
-			// // // iterator erase (iterator first, iterator last);
+			// void insert (iterator position, InputIterator first, InputIterator last);
+			// iterator erase (iterator position);
+			// iterator erase (iterator first, iterator last);
 			void swap (vector& x);
-			void clear();
+			
+			// Erases all elements from the container. After this call, size()
+			// returns zero. 
+			void clear() {
+				for (size_type i = 0; i < _size; i++) {
+					_alloc.destroy(_p + i);
+				}
+				_size = 0;
+			}
 
-			/* Allocator */
+			/* --- Allocator --- */
 			allocator_type get_allocator() const;
 
-		
 		private:
-			allocator_type alloc;
-			size_type n;
-			value_type val;
-			T *arr;
-			
+			allocator_type	_alloc;
+			pointer			_p;
+			size_type		_size; // the number of elements
+			size_type		_capacity; // the number of elements that can be held in currently allocated storage
+
 	};
 
-	
 	/* Non-member function overloads */
 	template <class T, class Alloc>
 	bool operator== (const vector<T,Alloc>& lhs, const vector<T,Alloc>& rhs) {
