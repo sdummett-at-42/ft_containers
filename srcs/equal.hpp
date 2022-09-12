bfa8dfcfe7b0375c25e0f7ecb2de3b79906f8478
Stone <stone.dummett@gmail.com>
2022-09-12 17:17:20 +0200

    Implement ft::equal

--- srcs/equal.hpp
+++ srcs/equal.hpp
@@ -6,7 +6,7 @@
 /*   By: sdummett <sdummett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/16 14:12:03 by sdummett          #+#    #+#             */
-/*   Updated: 2022/07/05 17:11:34 by sdummett         ###   ########.fr       */
+/*   Updated: 2022/09/12 17:11:14 by sdummett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,19 +18,23 @@ namespace ft {
 	template <class InputIterator1, class InputIterator2>
 	bool equal (InputIterator1 first1, InputIterator1 last1,
 		InputIterator2 first2) {
-			(void)first1;
-			(void)last1;
-			(void)first2;
+		for (; first1 != last1; ++first1, ++first2) {
+		if (!(*first1 == *first2)) {
+			return false;
 		}
+	}
+    return true;	}
 
 	template <class InputIterator1, class InputIterator2, class BinaryPredicate>
 	bool equal (InputIterator1 first1, InputIterator1 last1,
 		InputIterator2 first2, BinaryPredicate pred) {
-			(void)first1;
-			(void)last1;
-			(void)first2;
-			(void)pred;
+		for (; first1 != last1; ++first1, ++first2) {
+			if (!pred(*first1, *first2)) {
+				return false;
+			}
 		}
+		return true;
+	}
 }
 
 #endif
