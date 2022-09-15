9ebbf1db11bcc90638193d7a8669a1b1813e2de8
Stone <stone.dummett@gmail.com>
2022-09-15 11:36:29 +0200

    Re-organize ft::map template class to work on it easily + add declaration of non member functions

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -6,14 +6,14 @@
 /*   By: sdummett <sdummett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/16 14:08:35 by sdummett          #+#    #+#             */
-/*   Updated: 2022/09/14 10:16:13 by sdummett         ###   ########.fr       */
+/*   Updated: 2022/09/15 10:43:22 by sdummett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include <iostream>
 
 # include "vector.hpp"
-// # include "map.hpp"
+# include "map.hpp"
 # include "stack.hpp"
 // # include "iterator_traits.hpp"
 // # include "reverse_iterator.hpp"
@@ -28,87 +28,9 @@
 #include <string>
 #include <stack>
 
-#define FT
-#define TESTED_NAMESPACE ft
-#define TESTED_TYPE int
-#define T_SIZE_TYPE typename TESTED_NAMESPACE::vector<T>::size_type
-
-template <typename T>
-void	printSize(TESTED_NAMESPACE::vector<T> const &vct, bool print_content = true)
-{
-	const T_SIZE_TYPE size = vct.size();
-	const T_SIZE_TYPE capacity = vct.capacity();
-	const std::string isCapacityOk = (capacity >= size) ? "OK" : "KO";
-	// Cannot limit capacity's max value because it's implementation dependent
-
-	std::cout << "size: " << size << std::endl;
-	std::cout << "capacity: " << isCapacityOk << std::endl;
-	std::cout << "max_size: " << vct.max_size() << std::endl;
-	if (print_content)
-	{
-		typename TESTED_NAMESPACE::vector<T>::const_iterator it = vct.begin();
-		typename TESTED_NAMESPACE::vector<T>::const_iterator ite = vct.end();
-		std::cout << std::endl << "Content is:" << std::endl;
-		for (; it != ite; ++it)
-			std::cout << "- " << *it << std::endl;
-	}
-	std::cout << "###############################################" << std::endl;
-}
-
-void	prepost_incdec(TESTED_NAMESPACE::vector<TESTED_TYPE> &vct)
-{
-
-	TESTED_NAMESPACE::vector<TESTED_TYPE>::iterator it = vct.begin();
-	TESTED_NAMESPACE::vector<TESTED_TYPE>::iterator it_tmp;
-
-	std::cout << "Pre inc" << std::endl;
-	it_tmp = ++it;
-	std::cout << *it_tmp << " | " << *it << std::endl;
-
-	std::cout << "Pre dec" << std::endl;
-	it_tmp = --it;
-	std::cout << *it_tmp << " | " << *it << std::endl;
-
-	std::cout << "Post inc" << std::endl;
-	it_tmp = it++;
-	std::cout << *it_tmp << " | " << *it << std::endl;
-
-	std::cout << "Post dec" << std::endl;
-	it_tmp = it--;
-	std::cout << *it_tmp << " | " << *it << std::endl;
-	std::cout << "###############################################" << std::endl;
-}
-
-
 int main() {
 
-	const int size = 5;
-	TESTED_NAMESPACE::vector<TESTED_TYPE> vct(size);
-	TESTED_NAMESPACE::vector<TESTED_TYPE>::iterator it = vct.begin();
-	TESTED_NAMESPACE::vector<TESTED_TYPE>::const_iterator ite = vct.begin();
-
-	for (int i = 0; i < size; ++i)
-		it[i] = (size - i) * 5;
-	prepost_incdec(vct);
-
-	it = it + 5;
-	it = 1 + it;
-	it = it - 4;
-	std::cout << *(it += 2) << std::endl;
-	std::cout << *(it -= 1) << std::endl;
-
-	*(it -= 2) = 42;
-	*(it += 2) = 21;
-
-	std::cout << "const_ite +=: " << *(ite += 2) << std::endl;
-	std::cout << "const_ite -=: " << *(ite -= 2) << std::endl;
-
-	std::cout << "(it == const_it): " << (ite == it) << std::endl;
-	std::cout << "(const_ite - it): " << (ite - it) << std::endl;
-	std::cout << "(ite + 3 == it): " << (ite + 3 == it) << std::endl;
-
-	printSize(vct, true);
-	return (0);
+	
 
 	// std::cout << ft::is_integral< char >::value << "\n";
 
--- srcs/map.hpp
+++ srcs/map.hpp
@@ -6,51 +6,75 @@
 /*   By: sdummett <sdummett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/16 14:12:03 by sdummett          #+#    #+#             */
-/*   Updated: 2022/07/05 17:04:47 by sdummett         ###   ########.fr       */
+/*   Updated: 2022/09/15 11:32:15 by sdummett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # ifndef MAP_HPP
 # define MAP_HPP
 
-#include "pair.hpp"
+#include <functional>	// Required for std::less<Key>
+#include <memory>		// Required for std::allocator<T>
+#include "pair.hpp"		// Required for ft::pair<const key_type,mapped_type>
 
 namespace ft {
 
-	template < class Key,                                 // map::key_type
-           class T,                                       // map::mapped_type
-           class Compare = std::less<Key>,                     // map::key_compare
-           class Alloc = std::allocator<std::pair<const Key,T> >    // map::allocator_type
-           >
-	class map {
+	template<
+		class Key,
+		class T,
+		class Compare = std::less<Key>,
+		class Alloc = std::allocator<ft::pair<const Key, T> > > 
+		class map {
+
 		public:
-			/* Typedefs */
+
+			/* ------------- Typedefs ------------- */
+
 			typedef Key key_type;
 			typedef T mapped_type;
 			typedef ft::pair<const key_type,mapped_type> value_type;
+			typedef std::size_t size_type;
+			typedef std::ptrdiff_t difference_type;
 			typedef Compare key_compare;
-			// typdef ... value_compare;
 			typedef Alloc allocator_type;
-			typedef typename allocator_type::reference reference;
-			typedef typename allocator_type::const_reference const_reference;
+			typedef value_type& reference;
+			typedef const value_type& const_reference;
 			typedef typename allocator_type::pointer pointer;
 			typedef typename allocator_type::const_pointer const_pointer;
-			typedef size_t size_type;
+			// typedef ft::bidirectional_iterator<value_type> iterator;
+			// typedef ft::bidirectional_iterator<const value_type> const_iterator;
+			// typedef ft::reverse_iterator<iterator> reverse_iterator;
+			// typedef ft::reverse_iterator<const_iterator> const_reverse_iterator; 
 
-			/* Constructors */
-			explicit map (const key_compare& comp = key_compare(),
-              const allocator_type& alloc = allocator_type());
+
+			/* ------------- Constructors ------------- */
+
+			explicit map (	const key_compare& comp = key_compare(),
+							const allocator_type& alloc = allocator_type());
 			template <class InputIterator>
 				map (InputIterator first, InputIterator last,
 				const key_compare& comp = key_compare(),
 				const allocator_type& alloc = allocator_type());
 			map (const map& x);
 
-			/* Destructor */
-			~map();
-			/* operator= */
+
+			/* ------------- operator= ------------- */
+
 			map& operator= (const map& x);
-			/* Iterators */
+
+
+			/* ------------- Allocator ------------- */
+
+			allocator_type get_allocator() const;
+
+
+			/* ------------- Destructor ------------- */
+
+			~map();
+
+
+			/* ------------- Iterators ------------- */
+
 			// iterator begin();
 			// const_iterator begin() const;
 			// iterator end();
@@ -59,26 +83,50 @@ namespace ft {
 			// const_reverse_iterator rbegin() const;
 			// reverse_iterator rend();
 			// const_reverse_iterator rend() const;
-			/* Capacity */
+
+
+			/* ------------- Capacity ------------- */
+
 			bool empty() const;
 			size_type size() const;
 			size_type max_size() const;
-			/* Element access */
+
+
+			/* ------------- Element access ------------- */
+
+			mapped_type& at( const key_type& key );
+			const mapped_type& at( const key_type& key ) const;
 			// mapped_type& operator[] (const key_type& k);
-			/* Modfiers */
-			// pair<iterator,bool> insert (const value_type& val);
-			// // iterator insert (iterator position, const value_type& val);
+
+
+			/* ------------- Modfiers ------------- */
+
+			// ft::pair<iterator,bool> insert (const value_type& val);
+			// iterator insert (iterator position, const value_type& val);
 			template <class InputIterator>
 				void insert (InputIterator first, InputIterator last);
+
+			// Notes : erase with iterator as parameter have
+			// 			different return type on cplusplus.com
+			//			and cppreference.com
 			// void erase (iterator position);
+			// void erase (iterator first, iterator last);
+			// 			OR
+			// iterator erase( iterator pos );
+			// iterator erase( iterator first, iterator last );
 			// size_type erase (const key_type& k);
-			// // void erase (iterator first, iterator last);
 			void swap (map& x);
 			void clear();
-			/* Observers */
+
+
+			/* ------------- Observers ------------- */
+
 			key_compare key_comp() const;
 			// value_compare value_comp() const;
-			/* Operations */
+
+
+			/* ------------- Lookup ------------- */
+
 			// iterator find (const key_type& k);
 			// const_iterator find (const key_type& k) const;
 			size_type count (const key_type& k) const;
@@ -86,11 +134,42 @@ namespace ft {
 			// const_iterator lower_bound (const key_type& k) const;
 			// iterator upper_bound (const key_type& k);
 			// const_iterator upper_bound (const key_type& k) const;
-			// // pair<const_iterator,const_iterator> equal_range (const key_type& k) const;
-			// // pair<iterator,iterator> equal_range (const key_type& k);
-			/* Allocator */
-			allocator_type get_allocator() const;
+			// ft::pair<iterator,iterator> equal_range (const key_type& k);
+			// ft::pair<const_iterator,const_iterator> equal_range (const key_type& k) const;
+
 	};
+
+
+	/* ------------- Non-member function ------------- */
+
+	template< class Key, class T, class Compare, class Alloc >
+	void swap( ft::map<Key,T,Compare,Alloc>& lhs,
+           ft::map<Key,T,Compare,Alloc>& rhs );
+
+	template< class Key, class T, class Compare, class Alloc >
+	bool operator==( const ft::map<Key,T,Compare,Alloc>& lhs,
+                 const ft::map<Key,T,Compare,Alloc>& rhs );
+
+	template< class Key, class T, class Compare, class Alloc >
+	bool operator!=( const ft::map<Key,T,Compare,Alloc>& lhs,
+					const ft::map<Key,T,Compare,Alloc>& rhs );
+
+	template< class Key, class T, class Compare, class Alloc >
+	bool operator<( const ft::map<Key,T,Compare,Alloc>& lhs,
+					const ft::map<Key,T,Compare,Alloc>& rhs );
+
+	template< class Key, class T, class Compare, class Alloc >
+	bool operator<=( const ft::map<Key,T,Compare,Alloc>& lhs,
+					const ft::map<Key,T,Compare,Alloc>& rhs );
+
+	template< class Key, class T, class Compare, class Alloc >
+	bool operator>( const ft::map<Key,T,Compare,Alloc>& lhs,
+					const ft::map<Key,T,Compare,Alloc>& rhs );
+
+	template< class Key, class T, class Compare, class Alloc >
+	bool operator>=( const ft::map<Key,T,Compare,Alloc>& lhs,
+					const ft::map<Key,T,Compare,Alloc>& rhs );
+
 }
 
 #endif
